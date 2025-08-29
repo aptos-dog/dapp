3f7bb2c4bdac7bbb6d37e7948cbf415953316c8e
aptos-dog <aptosdog13@gmail.com>
2025-08-29 08:46:19 +0000

    update

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -76,7 +76,7 @@ const w = normWallet(wallet);
  */
 export async function PUT(req: Request) {
   try {
-    const { wallet, username, referral_code } = await req.json();
+    git 
     const w = normWallet(wallet);
 
     if (!w || !username) {
